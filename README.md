75660fee1cfa6b16aef05ca55a5c88e6c8d23e1c
Jeremy Yu <jeremy.yu@wustl.edu>
2018-04-16 14:00:23 -0500

    Working on JS and HTML

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # IoT-Smart-Tarp
 A final project for my CSE 222: Internet of Things class at Washington University in St. Louis
+
+### Conceptual Backing
+
+### Back-End Design
+
+### Front-End Design
+
+### Build Process
+
+### Documentation and References
+
+[Bootstrap](https://getbootstrap.com/docs/4.1/getting-started/introduction/)
---
+++ ui/Tarp.js
@@ -0,0 +1,55 @@
+
+var company_title;
+var control_panel_btn
+var photos_btn
+var settings_btn
+
+var photos_div
+var settings_div
+var home_div
+var user_div
+
+
+function goToSettings() {
+
+}
+
+function goToUserProfile() {
+
+}
+
+function goToControlPanel() {
+
+}
+
+function goToPhotos() {
+
+}
+
+function hideForCP() {
+
+}
+
+function hideForSettings() {
+
+}
+
+function hideForUser() {
+
+}
+
+function hideForPhotos() {
+
+}
+
+$(window).on('load', function() {
+  company_title = $("#company_title")[0];
+  control_panel_btn = $("#cp_btn")[0];
+  photos_btn = $("#photos_btn")[0];
+  settings_btn = $("#settings_btn")[0];
+
+  home_div = $("#home_div")[0];
+  user_div = $("#user_div")[0];
+  photos_div = $("#photos_div")[0];
+  settings_div = $("#settings_div")[0];
+});
