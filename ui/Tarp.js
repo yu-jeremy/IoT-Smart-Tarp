b2ad073d1737e65f9b5331ed9e8ba6d53e1e078c
Jeremy Yu <jeremy.yu@wustl.edu>
2018-04-16 15:16:34 -0500

    Finished movement from one page to the next

--- ui/Tarp.js
+++ ui/Tarp.js
@@ -1,8 +1,20 @@
 
-var company_title;
-var control_panel_btn
+var company_title
 var photos_btn
 var settings_btn
+var profile_btn
+
+var photosToCp
+var photosToUser
+var photosToSettings
+
+var settingsToCp
+var settingsToUser
+var settingsToPhotos
+
+var userToCp
+var userToSettings
+var userToPhotos
 
 var photos_div
 var settings_div
@@ -11,46 +23,72 @@ var user_div
 
 
 function goToSettings() {
-  photos_div.hide();
-  home_div.hide();
-  user_div.hide();
+  photos_div.style.display = "none";
+  home_div.style.display = "none";
+  user_div.style.display = "none";
+  settings_div.style.display = "block";
 }
 
 function goToUserProfile() {
-  home_div.hide();
-  photos_div.hide();
-  settings_div.hide();
+  home_div.style.display = "none";
+  photos_div.style.display = "none";
+  settings_div.style.display = "none";
+  user_div.style.display = "block";
 }
 
 function goToControlPanel() {
-  photos_div.hide();
-  user_div.hide();
-  settings_div.hide();
+  photos_div.style.display = "none";
+  user_div.style.display = "none";
+  settings_div.style.display = "none";
+  home_div.style.display = "block";
 }
 
 function goToPhotos() {
-  user_div.hide();
-  settings_div.hide();
-  home.hide();
+  user_div.style.display = "none";
+  settings_div.style.display = "none";
+  home_div.style.display = "none";
+  photos_div.style.display = "block";
 }
 
-$(window).on('load', function() {
-  company_title = $("#company_title")[0];
-  control_panel_btn = $("#cp_btn")[0];
-  photos_btn = $("#photos_btn")[0];
-  settings_btn = $("#settings_btn")[0];
+window.addEventListener("load", function() {
+  company_title = document.getElementById("company_title");
+  control_panel_btn = document.getElementById("cp_btn");
+  photos_btn = document.getElementById("photos_btn");
+  settings_btn = document.getElementById("settings_btn");
+  profile_btn = document.getElementById("profile_btn");
+
+  photosToCp = document.getElementById("photosToCp");
+  photosToUser = document.getElementById("photosToUser");
+  photosToSettings = document.getElementById("photosToSettings");
+
+  settingsToCp = document.getElementById("settingsToCp");
+  settingsToUser = document.getElementById("settingsToUser");
+  settingsToPhotos = document.getElementById("settingsToPhotos");
+
+  userToCp = document.getElementById("userToCp");
+  userToSettings = document.getElementById("userToSettings");
+  userToPhotos = document.getElementById("userToPhotos");
 
   home_div = $("#home_div")[0];
   user_div = $("#user_div")[0];
   photos_div = $("#photos_div")[0];
   settings_div = $("#settings_div")[0];
 
+  goToControlPanel();
 
+  profile_btn.addEventListener("click", goToUserProfile);
+  photos_btn.addEventListener("click", goToPhotos);
+  settings_btn.addEventListener("click", goToSettings);
 
+  photosToCp.addEventListener("click", goToControlPanel);
+  photosToUser.addEventListener("click", goToUserProfile);
+  photosToSettings.addEventListener("click", goToSettings);
 
-  control_panel_btn.on('click', function() {
-    goToControlPanel();
-  })
+  settingsToCp.addEventListener("click", goToControlPanel);
+  settingsToUser.addEventListener("click", goToUserProfile);
+  settingsToPhotos.addEventListener("click", goToPhotos);
 
-  
+  userToCp.addEventListener("click", goToControlPanel);
+  userToSettings.addEventListener("click", goToSettings);
+  userToPhotos.addEventListener("click", goToPhotos);
 });
